9e3b3b1243ed323c0cb8c5d51c6c975e2659116e
JoseUrtubey <joseaurtubey@gmail.com>
2024-04-27 15:31:14 -0300

    added file with provinces and countries

---
+++ TP_integrador.py
@@ -0,0 +1,91 @@
+cp = input ("Ingrese el código postal del lugar de destino: ").lower()
+direccion = input("Dirección del lugar de destino: ")
+tipo = int(input("Tipo de envío (id entre 0 y 6 - ver tabla 2 en el enunciado): "))
+pago = int(input("Forma de pago (1: efectivo - 2: tarjeta): "))
+destino = ""
+provincia = ""
+final=""
+inicial = ""
+
+if len(cp) == 4:
+   destino == "Bolivia"
+elif len(cp) == 5:
+    destino= "Uruguay"
+elif len(cp) == 9:
+    destino= "Brazil"
+elif len(cp) == 8:
+    destino= "Argentina"
+elif len(cp) == 7:
+    destrino= "Chile"
+elif len(cp) == 6:
+    destino= "Paraguay"
+
+if destino == "Argentina":
+    if 'a' in cp[0]:
+        provincia = "Salta"
+    elif "b" in cp[0] and "a" in cp[1]:
+        provincia = "buenos Aires"
+    elif  "c" in cp[0]:
+        provincia = "CABA"
+    elif 'd' in cp[0]:
+        provincia  = "San Luis"
+    elif "e" in cp[0]:
+        provincia = "Entre Ríos"
+    elif  "f" in cp[0]:
+        provincia = "La Rioja"
+    elif 'g' in cp[0]:
+        provincia  = "Santiago del Estero"
+    elif 'h' in cp[0]:
+        provincia  = "Chaco"
+    elif  "j" in cp[0]:
+        provincia = "San Juan"
+    elif 'k' in cp[0]:
+        provincia  = "Catamarca"
+    elif 'l' in cp[0]:
+        provincia  = "La Pampa"
+    elif  "m" in cp[0]:
+        provincia = "Mendoza"
+    elif 'p' in cp[0]:
+        provincia  = "Formosa"
+    elif 'q' in cp[0]:
+        provincia  = "Neuquén"
+    elif  "m" in cp[0]:
+        provincia = "Mendoza"
+    elif  "n" in cp[0]:
+        provincia = "Misiones"
+    elif 'p' in cp[0]:
+        provincia  = "Formosa"
+    elif 'q' in cp[0]:
+        provincia  = "Neuquén"
+    elif  "r" in cp[0]:
+        provincia = "Río Negro"
+    elif 's' in cp[0]:
+        provincia  = "Santa Fe"
+    elif 't' in cp[0]:
+        provincia  = "Tucumán"
+    elif  "u" in cp[0]:
+        provincia = "Chubut"
+    elif 'v' in cp[0]:
+        provincia  = "Tierra del Fuego, Antártida e Islas del Atlántico Sur"
+    elif 'w' in cp[0]:
+        provincia  = "Corrientes"
+    elif 'x' in cp[0]:
+        provincia = "cordoba"
+    elif 'y' in cp[0]:
+        provincia = "Jujuy"
+    elif 'z' in cp[0]:
+        provincia = "Santa Cruz"
+    else:
+        print("Error en la provincia")
+
+
+print("Provincia destino:", provincia)
+    
+
+
+
+""" print("País de destino del envío:", destino)
+print("Importe inicial a pagar:", inicial)
+print("Importe final a pagar:", final) """
+
+
